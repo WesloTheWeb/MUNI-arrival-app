39c9d5f89b13669aaf80d5e3d2e899ce09a849ec
Wesley <wesleyadwebster@gmail.com>
2022-09-23 04:16:51 -0500

    CSS properties added and attempt at SVG

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import brand from './svg/muniapp.svg';
+import logo from './muni-logo.png';
+
+export { brand };
+export { logo };
---
+++ src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Dashboard.module.scss';
+
+const { dashboardContainer } = classes;
+
+const Dashboard = (props) => {
+    return (
+        <section className={dashboardContainer}>
+            
+        </section>
+    );
+};
+
+
+export default Dashboard;
