4e819115771697c2a5708f67323f8b7372f016b0
Wesley <wesleyadwebster@gmail.com>
2022-09-24 01:19:27 -0500

    dashboard statically added css styles. Refactor later

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import classes from './Dashboard.module.scss';
 
-const { dashboardContainer } = classes;
+const { dashboardContainer, prediction, predictionNearest, predictionMinutes, predictionLabel
+} = classes;
+
+//TODO: Breakdown the CSS to SCSS module.
 
 const Dashboard = (props) => {
     return (
-        <section className={dashboardContainer}>
-            <h2>content</h2>
-        </section>
+        // <section className={dashboardContainer}>
+        //     <div className={prediction}>
+        //         <div className={predictionNearest}>
+        //             <div className={predictionMinutes}>6</div>
+        //             <div className={predictionLabel}>minutes</div>
+        //         </div>
+        //     </div>
+        // </section>
+        <div className="view view-predictions">
+            <div className="predictions">
+                <div className="prediction prediction--nearest">
+                    <div className="prediction__minutes">6</div>
+                    <div className="prediction__label">minutes</div>
+                </div>
+                <div className="prediction">
+                    <div className="prediction__minutes">11</div>
+                </div>
+                <div className="prediction">
+                    <div className="prediction__minutes">18</div>
+                </div>
+            </div>
+        </div>
     );
 };
 
